336b42beae956a47dc6c50d5b511df15c99746be
Satit Rianpit <siteslave@msn.com>
2021-11-22 13:56:57 +0700

    add color swatch

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
+// Generate swatch color
+import 'generate_color.dart';
 import 'pages/home.dart';
 
 void main() {
   runApp(Main());
 }
 
+// Start theme
+final ThemeData theme = ThemeData(
+  primarySwatch: generateMaterialColor(Palette.primary),
+);
+
+class Palette {
+  static const Color primary = Color(0xFF003e70);
+}
+// End theme
+
 class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
+  Main({Key? key}) : super(key: key);
+
+  MaterialColor primarySwatch = generateMaterialColor(Palette.primary);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Demo App",
+      theme: ThemeData(
+        primarySwatch: primarySwatch,
+      ),
       home: HomePage(),
     );
   }
