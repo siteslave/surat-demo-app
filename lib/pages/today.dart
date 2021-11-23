bd2ff7037d571149aee991edf4814fa1b56e35d5
Satit Rianpit <siteslave@msn.com>
2021-11-23 14:49:56 +0700

    add loading

--- lib/pages/today.dart
+++ lib/pages/today.dart
@@ -13,11 +13,15 @@ class TodayPage extends StatefulWidget {
 
 class _TodayPageState extends State<TodayPage> {
   List users = [];
+  bool isLoading = true;
 
   Future getUsers() async {
     try {
       var url = Uri.parse('https://randomuser.me/api/?results=50');
       var res = await http.get(url);
+      setState(() {
+        isLoading = false;
+      });
       if (res.statusCode == 200) {
         var jsonResponse = convert.jsonDecode(res.body) as Map<String, dynamic>;
         setState(() {
@@ -27,6 +31,9 @@ class _TodayPageState extends State<TodayPage> {
         print("Network/Server error.");
       }
     } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
       print(error);
     }
   }
@@ -40,33 +47,44 @@ class _TodayPageState extends State<TodayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          var user = users[index];
-          var fullname =
-              "${user["name"]["title"]}${user["name"]["first"]} ${user["name"]["last"]}";
-          var email = user["email"];
-          var registerDate = user["registered"]["date"];
-          var gender = user["gender"];
-          var age = user["dob"]["age"];
-          Color color =
-              age <= 50 ? Colors.green.shade100 : Colors.orange.shade100;
-          var imageUrl = gender == "male"
-              ? "assets/images/man.png"
-              : "assets/images/woman.png";
+      body: Column(
+        children: [
+          isLoading
+              ? LinearProgressIndicator(
+                  minHeight: 5,
+                )
+              : Container(),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                var user = users[index];
+                var fullname =
+                    "${user["name"]["title"]}${user["name"]["first"]} ${user["name"]["last"]}";
+                var email = user["email"];
+                var registerDate = user["registered"]["date"];
+                var gender = user["gender"];
+                var age = user["dob"]["age"];
+                Color color =
+                    age <= 50 ? Colors.green.shade100 : Colors.orange.shade100;
+                var imageUrl = gender == "male"
+                    ? "assets/images/man.png"
+                    : "assets/images/woman.png";
 
-          return WidgetItemToday(
-            onTab: () {
-              print("$fullname");
-            },
-            imageUrl: imageUrl,
-            storeName: "$fullname",
-            address: "$email",
-            checkinTime: "$registerDate",
-            color: color,
-          );
-        },
-        itemCount: users.length,
+                return WidgetItemToday(
+                  onTab: () {
+                    print("$fullname");
+                  },
+                  imageUrl: imageUrl,
+                  storeName: "$fullname",
+                  address: "$email",
+                  checkinTime: "$registerDate",
+                  color: color,
+                );
+              },
+              itemCount: users.length,
+            ),
+          ),
+        ],
       ),
     );
   }
